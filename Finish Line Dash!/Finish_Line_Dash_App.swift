43a15f26de593b1b3276b9162438f0e3eeb6b9fc
Dan Stoakes <dan.stoakes8@gmail.com>
2021-08-14 12:10:08 +0100

    Initial Commit

---
+++ Finish Line Dash!/Finish_Line_Dash_App.swift
@@ -0,0 +1,17 @@
+//
+//  Finish_Line_Dash_App.swift
+//  Finish Line Dash!
+//
+//  Created by Dan Stoakes on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Finish_Line_Dash_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
